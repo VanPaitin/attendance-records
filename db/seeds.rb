8b0d14020ea40dcaa555b34244d3a3970a4c6032
Mayowa Pitan <mayowa.pitan@andela.com>
2019-12-23 03:22:12 +0100

    create service types

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,18 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+services = [
+  "Thanksgiving Service", "1st Clearwater Service", "2nd Clearwater Service",
+  "Anointing Service", "Prayer Clinic", "Holy Communion", "Congregational Vigil",
+  "Digging Deep", "Faith Clinic", "Special Service"
+]
+
+services.map! do |service|
+  { name: service }
+end
+
+Service.create!(services)
+
+puts 'Successfully created service types!'
