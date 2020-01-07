a522bd12b825c5c7335f184ad210f754a4341960
Mayowa Pitan <mayowa.pitan@andela.com>
2020-01-07 07:06:43 +0100

    show more data on adult page

--- app/javascript/Attendance.tsx
+++ app/javascript/Attendance.tsx
@@ -1,5 +1,18 @@
 import React from 'react';
+import moment from 'moment';
 
 export default ({ record }) => (
-  <tr><td>{record.male}</td><td>{record.female}</td></tr>
+  <tr>
+    <td>{moment(record.day).format('dddd, MMMM Do YYYY')}</td>
+    <td>{record.service_name}</td>
+    <td>{record.male}</td>
+    <td>{record.female}</td>
+    <td>{record.children}</td>
+    <td>{record.online.facebook}</td>
+    <td>{record.online.youtube}</td>
+    <td>{record.newcomers.male}</td>
+    <td>{record.newcomers.female}</td>
+    <td>{record.decisions.male}</td>
+    <td>{record.decisions.female}</td>
+  </tr>
 )
