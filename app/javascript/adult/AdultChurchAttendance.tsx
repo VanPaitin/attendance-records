464966117265ff780d1910dadba96e6f4c7e8e13
Mayowa Pitan <mayowa.pitan@andela.com>
2020-01-27 02:15:49 +0100

    animate popover

--- app/javascript/adult/AdultChurchAttendance.tsx
+++ app/javascript/adult/AdultChurchAttendance.tsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import { Route, RouteProps, Link, withRouter } from 'react-router-dom';
-import $ from 'jquery';
 import axios from 'axios';
 import Button from 'react-bootstrap/Button';
+import styled from 'styled-components';
 
 import FormModal from './FormModal';
 import ActionsPopover from './ActionsPopover';
 import AttendancesTable from './AttendancesTable';
+
+const Container = styled.div`
+  &.container {
+    margin-top: 50px;
+    max-width: 1170px;
+  }
+`;
 
 type MaleFemaleType = {
   male: number
@@ -41,15 +48,11 @@ interface State {
 }
 
 class AdultChurchAttendance extends React.Component<RouteProps, State> {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      records: [],
-      popoverId: null,
-      popoverPosition: { left: null, top: null }
-    }
-  }
+  state = {
+    records: [],
+    popoverId: null,
+    popoverPosition: { left: null, top: null }
+  };
 
   fetchRecords = () => {
     axios.get('/attendances', { params: { mode: 'adult' }}).then(({ data }) => {
@@ -65,29 +68,29 @@ class AdultChurchAttendance extends React.Component<RouteProps, State> {
     let confirmation = confirm('Are you sure to delete the record?');
 
     if (confirmation) {
-      let token = $('meta[name="csrf-token"]').attr("content");
-      axios.delete(
-        `/attendances/${id}?mode=adult`,
-        { data: { authenticity_token: token } }
-      ).then(() => {
-        this.setState({
+      let meta = document.querySelector<HTMLElement>('meta[name="csrf-token"]') as HTMLMetaElement;
+      let token = meta.content;
+
+      axios.delete(`/attendances/${id}?mode=adult`, { data: { authenticity_token: token } })
+        .then(() => this.setState({
           popoverId: null,
           records: this.state.records.filter(record => record.id !== id)
-        });
-      })
+        }))
     }
   };
 
   showPopover = (e, id) => {
     e.stopPropagation();
     e.persist();
-    if (this.state.popoverId == id) {
-      this.clearPopoverId()
-    } else {
-      let clientWidth = document.body.clientWidth;
-      let minLeftOffset = clientWidth - 200;
-      let leftOffset = e.clientX <= minLeftOffset ? e.clientX : minLeftOffset;
-      this.setState({ popoverPosition: { left: leftOffset, top: e.clientY }, popoverId: id})
+    this.clearPopoverId();
+
+    if (this.state.popoverId !== id) {
+      setTimeout(() => {
+        let clientWidth = document.body.clientWidth;
+        let minLeftOffset = clientWidth - 200;
+        let leftOffset = e.clientX <= minLeftOffset ? e.clientX : minLeftOffset;
+        this.setState({ popoverPosition: { left: leftOffset, top: e.clientY }, popoverId: id})
+      }, 100)
     }
   };
 
@@ -97,14 +100,12 @@ class AdultChurchAttendance extends React.Component<RouteProps, State> {
 
   render() {
     return (
-      <div className='container'  onClick={this.clearPopoverId}>
-        {this.state.popoverId &&
+      <Container className='container'  onClick={this.clearPopoverId}>
         <ActionsPopover
           recordId={this.state.popoverId}
           position={this.state.popoverPosition}
           removeRecord={this.removeRecord}
           clearPopover={this.clearPopoverId}/>
-        }
 
         <h3>Recent Records</h3>
 
@@ -123,7 +124,7 @@ class AdultChurchAttendance extends React.Component<RouteProps, State> {
         <Route path={`${this.props.match.path}attendance/:id/edit`}>
           <FormModal fetchRecords={this.fetchRecords}/>
         </Route>
-      </div>
+      </Container>
     )
   }
 }
